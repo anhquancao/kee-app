f617c02704d7382a5c829cd6f1cc85f5893506fe
Cao Anh Quan <aquancva@gmail.com>
2017-03-01 11:11:02 +0100

    Upload code

--- public/client/src/apis/authApi.js
+++ public/client/src/apis/authApi.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
-import {API_BASE_URL} from '../constants/.env';
+import env from '../constants/.env';
 
 export default {
     login(email, password){
-        return axios.post(API_BASE_URL + 'login', {email, password});
+        return axios.post(env.API_BASE_URL + 'login', {email, password});
     },
     register(email, password, name, password_confirmation){
-        return axios.post(API_BASE_URL + 'register', {email, password, name, password_confirmation});
+        return axios.post(env.API_BASE_URL + 'register', {email, password, name, password_confirmation});
     },
     refreshToken(token){
-        return axios.get(API_BASE_URL + "refresh-token?token=" + token);
+        return axios.get(env.API_BASE_URL + "refresh-token?token=" + token);
     }
 };
--- public/client/src/constants/.env.js
+++ public/client/src/constants/.env.js
@@ -1,1 +1,7 @@
-export const API_BASE_URL = 'http://kee.app/api/';
+let API_BASE_URL = 'http://ec2-52-76-123-135.ap-southeast-1.compute.amazonaws.com/api/';
+if (process.env.NODE_ENV === "development") {
+    API_BASE_URL = 'http://kee.app/api/';
+}
+export default {
+    API_BASE_URL
+};
